4907df87cf0795a7adfb79b57e16949ff0e776f8
choi-haram <62204475+choi-haram@users.noreply.github.com>
2022-08-31 13:31:48 +0900

    Create baekjoon-10908.java

---
+++ baekjoon-10908.java
@@ -0,0 +1,12 @@
+// baekjoon-10908.java
+// https://www.acmicpc.net/problem/10998
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String args[]){
+        Scanner in = new Scanner(System.in);
+        int A = in.nextInt();
+        int B = in.nextInt();
+        System.out.println(A*B);
+    }
+}
