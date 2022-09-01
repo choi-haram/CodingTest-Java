483d75524c424536380d4ea5c6166e4452e43736
choi-haram <62204475+choi-haram@users.noreply.github.com>
2022-09-01 13:20:11 +0900

    Create baekjoon-1008.java
    baekjoon-1008번 나눗셈 문제

---
+++ baekjoon-1008.java
@@ -0,0 +1,12 @@
+// baekjoon-1008.java
+// https://www.acmicpc.net/problem/1008
+import java.util.Scanner;
+
+public class Main{    
+    public static void main(String args[]){
+        Scanner in = new Scanner(System.in);
+        double A = in.nextInt();
+        double B = in.nextInt();
+        System.out.println(A/B);
+    }
+}
