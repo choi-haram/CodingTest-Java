187e04c9de7c7428327ed069626ccd5e40e0b843
choi-haram <62204475+choi-haram@users.noreply.github.com>
2022-09-01 13:48:41 +0900

    Create baekjoon-10926.java

---
+++ baekjoon-10926.java
@@ -0,0 +1,13 @@
+// baekjoon-10926.java
+// https://www.acmicpc.net/problem/10926
+import java.util.Scanner;
+public class Main{
+    public static void main(String args[]){
+        Scanner in = new Scanner(System.in);
+        String A = in.next();
+        System.out.println(A+"??!");
+    }
+}
+/* 배운점 
+오타 줄이기, Scanner 메소드 공부 더 하기, 자료형 제대로 구분하기 
+*/
