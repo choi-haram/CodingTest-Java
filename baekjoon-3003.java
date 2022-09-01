de8916b25991b9c758c7528a4217a9f79738f4f1
choi-haram <62204475+choi-haram@users.noreply.github.com>
2022-09-01 14:26:54 +0900

    Create baekjoon-3003.java

---
+++ baekjoon-3003.java
@@ -0,0 +1,21 @@
+// baekjoon-3003.java
+// https://www.acmicpc.net/problem/3003
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String args[]){
+        Scanner in = new Scanner(System.in);
+        int A = in.nextInt();
+        int B = in.nextInt();
+        int C = in.nextInt();
+        int D = in.nextInt();
+        int E = in.nextInt();
+        int F = in.nextInt();
+        System.out.print(1-A+" ");
+        System.out.print(1-B+" ");
+        System.out.print(2-C+" ");
+        System.out.print(2-D+" ");
+        System.out.print(2-E+" ");
+        System.out.print(8-F+" ");
+    }
+}
