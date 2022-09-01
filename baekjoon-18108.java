652cab3a346f1bdc668dd09cf11955a0a7099c3c
choi-haram <62204475+choi-haram@users.noreply.github.com>
2022-09-01 13:58:12 +0900

    Create baekjoon-18108.java

---
+++ baekjoon-18108.java
@@ -0,0 +1,11 @@
+// baekjoon-18108.java
+// https://www.acmicpc.net/problem/18108
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String args[]){
+        Scanner in = new Scanner(System.in);
+        int A = in.nextInt();
+        System.out.println(A-543);
+    }
+}
