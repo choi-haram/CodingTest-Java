2901b9564f878390c6db88a0e794086ec9e88438
choi-haram <62204475+choi-haram@users.noreply.github.com>
2022-09-03 16:19:56 +0900

    Create baekjoon-10171.java

---
+++ baekjoon-10171.java
@@ -0,0 +1,12 @@
+// baekjoon-10171.java
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String args[]){
+    	Scanner Sc = new Scanner(System.in);
+    	System.out.println("\\    /\\ ");
+    	System.out.println(" )  ( ')");
+    	System.out.println("(  /  ) ");
+    	System.out.println(" \\(__)| ");
+    }
+}
