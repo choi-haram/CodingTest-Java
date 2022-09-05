f46ea4dd5603e73501ef4ffb1c273a2d023bcfb0
choi-haram <62204475+choi-haram@users.noreply.github.com>
2022-09-05 23:55:18 +0900

    Update baekjoon-2557.java

--- baekjoon-2557.java
+++ baekjoon-2557.java
@@ -1,8 +1,11 @@
-// 백준에서 자바로 코드작성 시 컴파일은 Main.java로 되기 때문에 클래스 명은 'Main'으로 하여야 한다.
 // backjoon-2557
 // https://www.acmicpc.net/problem/2557
+
 public class Main{
     public static void main(String args[]){
         System.out.println("Hello World!");
     }    
 }
+
+
+// 백준에서 자바로 코드작성 시 컴파일은 Main.java로 되기 때문에 클래스 명은 'Main'으로 하여야 한다.
