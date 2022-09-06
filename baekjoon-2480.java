9fa5ee6193ab2e895223264e56aa42aa3973635d
choi-haram <62204475+choi-haram@users.noreply.github.com>
2022-09-06 17:31:49 +0900

    Update baekjoon-2480.java
    알고리즘 공부 후 수정

--- baekjoon-2480.java
+++ baekjoon-2480.java
@@ -9,32 +9,22 @@ public class Main{
         int result = 0;
         int bestdice = 0;
         if(dice1 == dice2 && dice2 == dice3) {
-        	bestdice = dice1;
         	result = 10000+(dice1*1000);
-        } else if(dice1==dice3 && dice1!=dice2 && dice2!=dice3) { // 1 2 1
-        	bestdice = dice1; 
+        } else if(dice1==dice2 || dice1==dice3) { 
         	result = 1000+(dice1*100);
-        } else if(dice1==dice2 && dice1!=dice3 && dice2!=dice3) { // 1 1 2
-        	bestdice = dice1; 
-        	result = 1000+(dice1*100);
-        } else if(dice2==dice3 && dice1!=dice2 && dice1!=dice3){ //2 1 1
-        	bestdice = dice3; 
-        	result = 1000+(dice3*100);
+        } else if(dice2==dice3) { // 1 1 2 
+        	result = 1000+(dice2*100);
         } else { // 2 3 4
         	if(dice1>dice2) {
         		if(dice1>dice3) {
-        			bestdice = dice1;
         			result = dice1*100;
         		} else {
-        			bestdice = dice3;
         			result = dice3*100;
         		}
         	} else if(dice2>dice1) {
         		if(dice2>dice3) {
-        			bestdice = dice2;
         			result = dice2*100;
         		} else {
-        			bestdice = dice3;
         			result = dice3*100;
         		} 
         	}
