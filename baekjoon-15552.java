9a440bec6175d44397ef6371fc2be3329f0b2ca9
choi-haram <62204475+choi-haram@users.noreply.github.com>
2022-09-20 21:30:59 +0900

    Update baekjoon-15552.java

--- baekjoon-15552.java
+++ baekjoon-15552.java
@@ -1,1 +1,21 @@
 // baekjoon-15552.java
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main{
+	public static void main(String args[]) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		int T = Integer.parseInt(br.readLine());
+        for(int i=0; i<T; i++) {
+        	String[] strs = br.readLine().split(" ");
+        	int a = Integer.parseInt(strs[0]);
+            int b = Integer.parseInt(strs[1]);
+            bw.write(a+b+"\n");    	
+        }
+        bw.flush();
+	}
+}
