be10b78d64cff306dc36eb2bd382db2a7f8f9654
choi-haram <62204475+choi-haram@users.noreply.github.com>
2022-10-04 23:31:15 +0900

    Update baekjoon-1110.java

--- baekjoon-1110.java
+++ baekjoon-1110.java
@@ -5,3 +5,16 @@
     	3. 주어진 수 가장 오른쪽 자리 수와 앞에 구한 합의 가장 오른쪽 자리 수를 이어 붙여 새로운 수 생성
     	4. 처음 주어진 수로 돌아오기 까지의 사이클 구하기 
     	*/
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String args[]){
+    	Scanner Sc = new Scanner(System.in);
+    	
+    	char ch = Sc.next().charAt(0);
+    	System.out.println(ch+0);
+    	int num = (int)ch;
+    	System.out.println(num);
+    	
+    }
+}
