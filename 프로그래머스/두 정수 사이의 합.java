d23012aeabf4e5ad6b281ef099f46e8b03009e4c
choi-haram <62204475+choi-haram@users.noreply.github.com>
2023-01-19 15:07:33 +0900

    Create 두 정수 사이의 합.java

---
+++ 프로그래머스/두 정수 사이의 합.java
@@ -0,0 +1,19 @@
+// 두 정수 a, b가 주어졌을 때 a와 b 사이에 속한 모든 정수의 합을 리턴하는 함수, solution을 완성하세요.예를 들어 a = 3, b = 5인 경우, 3 + 4 + 5 = 12이므로 12를 리턴합니다.
+
+class Solution {
+    public long solution(int a, int b) {
+        long answer = 0;
+        int max = -1, min = -1;
+        if(a>b){
+            max = a;
+            min = b;
+        } else{
+            max = b;
+            min = a;
+        }
+        for(int i=min; i<=max; i++){
+            answer+= i;   
+        }
+        return answer;
+    }
+}
